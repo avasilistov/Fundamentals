eb4e44ca443c209aef66c9495c9b9c015f0a93f5
avasilistov <kvoroncova@gmail.com>
2022-08-03 21:03:08 -0500

    Tuples

--- players.py
+++ players.py
@@ -1,2 +1,10 @@
 players = ['charls', 'martina', 'frlorce', 'michael', 'elly']
 print(players[0:3])
+print(players[:2])
+print(players[3:])
+print(players[:])
+print(players[-3:])
+for player in players[:3]:
+    print(player.title())
+new_players = players[:]
+print(new_players)
---
+++ tuples_example.py
@@ -0,0 +1,4 @@
+dimensions = (200, 50)
+print(dimensions[0],dimensions[1])
+my = (3,)
+print(len(my))
